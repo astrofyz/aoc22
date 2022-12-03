7412c8ef208941309cf42bb7fa2e0ab4a39b2520
astrofyz <anparul97@gmail.com>
2022-12-03 09:18:55 -0600

    Day 03

---
+++ src/Day03.kt
@@ -0,0 +1,32 @@
+fun main() {
+    fun Char.priority(): Int {
+        return when (this.isUpperCase()){
+            true -> this - 'A' + 27
+            else -> this - 'a' + 1
+        }
+    }
+
+    fun part1(input: List<String>): Int {
+        val res = input.map { rucksack ->
+            rucksack.take(rucksack.length / 2).toSet() intersect rucksack.takeLast(rucksack.length / 2).toSet()
+        }.sumOf { it.elementAt(0).priority() }
+        return res
+    }
+
+    fun part2(input: List<String>): Int {
+        val res = input.chunked(3).map { rucksack -> rucksack[0]
+            .toSet()
+            .intersect(rucksack[1].toSet())
+            .intersect(rucksack[2].toSet()) }.sumOf { it.elementAt(0).priority() }
+        return res
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
