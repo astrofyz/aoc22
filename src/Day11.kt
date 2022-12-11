cbcf15eb8c7af674f8b5e0333e9c7bbdbf555dda
astrofyz <anparul97@gmail.com>
2022-12-11 12:26:25 -0600

    int to long

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,48 +1,45 @@
 fun main() {
 
     class Monkey(
-        var items: MutableList<Int> = mutableListOf(), var operation: String = "",
-        var test: Int = 1, var testTrue: Int = 0, var testFalse: Int = 1,
-        var business: Int = 0){
+        var items: MutableList<Long> = mutableListOf(), var operation: String = "",
+        var test: Long = 1, var testTrue: Long = 0, var testFalse: Long = 1,
+        var business: Long = 0){
     }
 
-    fun Int.update(operation: String): Int {
+    fun Long.update(operation: String): Long {
         return if (('+' in operation)&&(operation.any{it.isDigit()})) {
-            this + operation.filter { it.isDigit() }.toInt()
+            this + operation.filter { it.isDigit() }.toLong()
         } else if (('*' in operation)&&(operation.any{it.isDigit()})) {
-            this * operation.filter { it.isDigit() }.toInt()
+            this * operation.filter { it.isDigit() }.toLong()
         } else if ('+' in operation) {
             this + this
         } else {
-            if (this * this >= 2147483647){
-                println("greater than Int Max")
-            }
-//            println("$this, ${this * this}")
             this * this
         }
     }
 
-    fun Monkey.inspectionPart1(): Pair<Int, Int> {
-        var worryLevel: Int = this.items[0]
+    fun Monkey.inspectionPart1(): Pair<Long, Long> {
+        var worryLevel: Long = this.items[0]
         this.business++
         worryLevel = worryLevel.update(this.operation) / 3
-        if (worryLevel % this.test == 0.toInt()){
+        if (worryLevel % this.test == 0.toLong()){
             return Pair(worryLevel, this.testTrue)
         }
         else { return Pair(worryLevel, this.testFalse)}
     }
 
-    fun Monkey.inspectionPart2(lcm: Int): Pair<Int, Int> {
-        var worryLevel: Int = this.items[0]
+    fun Monkey.inspectionPart2(lcm: Long): Pair<Long, Long> {
+        var worryLevel: Long = this.items[0].toLong()
         this.business++
-        worryLevel = (worryLevel.update(this.operation)) % lcm
-        if (worryLevel % this.test == 0){
+        worryLevel = worryLevel.update(this.operation)
+        worryLevel = worryLevel % lcm
+        if (worryLevel % this.test == 0.toLong()){
             return Pair(worryLevel, this.testTrue)
         }
         else { return Pair(worryLevel, this.testFalse)}
     }
 
-    fun Monkey.throwing(other: Monkey, itemWorryLevel: Int){
+    fun Monkey.throwing(other: Monkey, itemWorryLevel: Long){
         this.items.removeAt(0)
         other.items.add(itemWorryLevel)
     }
@@ -58,29 +55,28 @@ fun main() {
                         Monkeys[currentIndex].items = line
                             .split(": ")[1]
                             .split(", ")
-                            .map{it.toInt()}
+                            .map{it.toLong()}
                             .toMutableList()
-                    contains("Test") -> Monkeys[currentIndex].test = line.filter { it.isDigit() }.toInt()
+                    contains("Test") -> Monkeys[currentIndex].test = line.filter { it.isDigit() }.toLong()
                     contains("Operation") -> Monkeys[currentIndex].operation = line.split("= ")[1]
-                    contains("If true") -> Monkeys[currentIndex].testTrue = line.filter { it.isDigit() }.toInt()
-                    contains("If false") -> Monkeys[currentIndex].testFalse = line.filter { it.isDigit() }.toInt()
+                    contains("If true") -> Monkeys[currentIndex].testTrue = line.filter { it.isDigit() }.toLong()
+                    contains("If false") -> Monkeys[currentIndex].testFalse = line.filter { it.isDigit() }.toLong()
                 }
             }
         }
         return Monkeys
     }
 
-    fun part1(input: List<String>): Int{
+    fun part1(input: List<String>): Long{
         var Monkeys = parseInput(input)
 
         repeat(20){
             for (monkey in Monkeys){
                 while (monkey.items.size > 0){
                     var (worryLevel, indexReceiver) = monkey.inspectionPart1()
-                    monkey.throwing(Monkeys[indexReceiver], worryLevel)
+                    monkey.throwing(Monkeys[indexReceiver.toInt()], worryLevel)
                     }
                 }
-//            Monkeys.mapIndexed { index, mnk -> println("Monkey $index: ${Monkeys[index].items}") }
             }
         Monkeys.mapIndexed { index, mnk -> println("Monkey $index inspected items ${Monkeys[index].business} times") }
         var business = Monkeys.map { it.business }.sortedDescending()
@@ -88,32 +84,30 @@ fun main() {
         }
 
 
-    fun part2(input: List<String>): Int{
+    fun part2(input: List<String>): Long{
         var Monkeys = parseInput(input)
 
         val lcm = Monkeys.map { it.test }.reduce{acc, testValue -> acc*testValue}
-        println("lcm $lcm")
 
-        repeat(20){
+        repeat(10000){
             for (monkey in Monkeys){
                 while (monkey.items.size > 0){
                     var (worryLevel, indexReceiver) = monkey.inspectionPart2(lcm)
-                    monkey.throwing(Monkeys[indexReceiver], worryLevel)
+                    monkey.throwing(Monkeys[indexReceiver.toInt()], worryLevel)
                 }
             }
         }
         Monkeys.mapIndexed { index, mnk -> println("Monkey $index inspected items ${Monkeys[index].business} times") }
         var business = Monkeys.map { it.business }.sortedDescending()
-        println(business)
         return business[0]*business[1]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day11_test")
-//    println(part1(testInput))
+    println(part1(testInput))
     println(part2(testInput))
 
     val input = readInput("Day11")
-//    println(part1(input))
+    println(part1(input))
     println(part2(input))
 }
